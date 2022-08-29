50d26b89aa54677bcc7c1e4c724870c4913e0ed4
andylamax <andylamax@programmer.net>
2022-08-30 00:03:41 +0300

    Adjusted test to pass on js as they do on jvm

--- core/src/commonTest/kotlin/LatLngAltSerializationTest.kt
+++ core/src/commonTest/kotlin/LatLngAltSerializationTest.kt
@@ -11,15 +11,15 @@ class LatLngAltSerializationTest {
 
     @Test
     fun should_serialize_a_lat_lng() {
-        val cord = LatLngAlt(10.0, -10.0, 5.0)
-        val json = """{"lat":10.0,"lng":-10.0,"alt":5.0}"""
+        val cord = LatLngAlt(10.01, -10.01, 5.01)
+        val json = """{"lat":10.01,"lng":-10.01,"alt":5.01}"""
         expect(Json.encodeToString(LatLngAltSerializer, cord)).toBe(json)
     }
 
     @Test
     fun should_deserialize_a_lat_lng() {
-        val json = """{"lat":5.0,"lng":0.01,"alt":5.5}"""
-        val cord = LatLngAlt(5.0, 0.01, 5.5)
+        val json = """{"lat":5.01,"lng":0.01,"alt":5.05}"""
+        val cord = LatLngAlt(5.01, 0.01, 5.05)
         expect(Json.decodeFromString(LatLngAltSerializer, json)).toBe(cord)
     }
 }
--- core/src/commonTest/kotlin/LatLngSerializationTest.kt
+++ core/src/commonTest/kotlin/LatLngSerializationTest.kt
@@ -9,15 +9,15 @@ class LatLngSerializationTest {
 
     @Test
     fun should_serialize_a_lat_lng() {
-        val cord = LatLng(10.0, -10.0)
-        val json = """{"lat":10.0,"lng":-10.0}"""
+        val cord = LatLng(10.01, -10.01)
+        val json = """{"lat":10.01,"lng":-10.01}"""
         expect(Json.encodeToString(LatLngSerializer, cord)).toBe(json)
     }
 
     @Test
     fun should_deserialize_a_lat_lng() {
-        val json = """{"lat":5.0,"lng":0.01}"""
-        val cord = LatLng(5.0, 0.01)
+        val json = """{"lat":5.01,"lng":0.01}"""
+        val cord = LatLng(5.01, 0.01)
         expect(Json.decodeFromString(LatLngSerializer, json)).toBe(cord)
     }
 }
